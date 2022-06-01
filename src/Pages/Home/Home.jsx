476ac49c94cade7c44335de529a02b8c2048e5f4
FilipeGuto <filipeguto95@gmail.com>
2022-06-01 16:43:30 -0300

    FEAT: Criação da pagina home e estilização

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
+
+import iconGoogle from "../../Images/google.png";
+import iconLoL from "../../Images/LoL.png";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
-};
+    <div className="home">
+      <div className="container">
+        <span className="container-g">
+          <a
+            className="g-icon"
+            href="https://www.google.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={iconGoogle} alt="icon Google" className="icon-g-img" />
+          </a>
+        </span>
+        <span className="container-l">
+          <button className="l-icon">
+            <img
+              src={iconLoL}
+              alt="icon League of Legends"
+              className="icon-l-img"
+            />
+          </button>
+        </span>
+      </div>
+    </div>
+  );
+}
